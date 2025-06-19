d3cfc34780b8b7472c1a2b4f89a62f980eab14ba
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-06-19 17:25:30 +0100

    Added empty initialiser and access methods.

--- dictcpp.hpp
+++ dictcpp.hpp
@@ -1,11 +1,71 @@
 #ifndef DICTCPP_HPP
 #define DICTCPP_HPP
+#include <set>
+#include <vector>
 
 namespace dictcpp {
 inline bool library_initialized() {
     return true;
 }
 
+template<typename Key, typename Value>
+class Dict {
+    std::vector<Key> keys;
+    std::vector<Value> values;
+
+    size_t get_index(const Key &key) const {
+        for (size_t i = 0; i < keys.size(); i++) {
+            if (keys[i] == key) {
+                return i;
+            }
+        }
+
+        throw std::out_of_range("Key not found");
+    }
+
+public:
+    // Initialise an empty dictionary
+    Dict(): keys({}), values({}) {
+    }
+
+    [[nodiscard]] bool empty() const {
+        return keys.empty();
+    }
+
+    [[nodiscard]] size_t size() const {
+        return keys.size();
+    }
+
+    const Value &operator[](const Key &key) const {
+        if (!std::set<Key>(keys.begin(), keys.end()).contains(key)) {
+            throw std::out_of_range("Key not found");
+        }
+
+        return values[get_index(key)];
+    }
+
+    Value &operator[](const Key &key) {
+        if (std::set<Key>(keys.begin(), keys.end()).contains(key)) {
+            const auto index = get_index(key);
+
+            return values[index];
+        }
+
+        keys.push_back(key);
+        values.push_back(Value());
+
+        return values.back();
+    }
+
+    Value &at(const Key &key) {
+        return operator[](key);
+    }
+
+    const Value &at(const Key &key) const {
+        return operator[](key);
+    }
+
+};
 }
 
 #endif //DICTCPP_HPP
--- tests/test_dict.cpp
+++ tests/test_dict.cpp
@@ -1,6 +1,79 @@
 #include "dictcpp.hpp"
 #include "tests/catch.hpp"
 
-TEST_CASE("Response") {
-    CHECK(dictcpp::library_initialized());
+TEST_CASE("Empty dict") {
+    const auto dict = dictcpp::Dict<char, char>();
+    REQUIRE(dict.empty());
+}
+
+TEST_CASE("Element assignment") {
+    auto dict = dictcpp::Dict<int, int>();
+    REQUIRE(dict.empty());
+
+    dict[2] = 20;
+    REQUIRE(dict.size() == 1);
+    CHECK(dict[2] == 20);
+    CHECK(dict.at(2) == 20);
+
+    dict[2] = 100;
+    REQUIRE(dict.size() == 1);
+    CHECK(dict[2] == 100);
+    CHECK(dict.at(2) == 100);
+
+    dict[2];
+    CHECK(dict.size() == 1);
+    CHECK(dict[2] == 100);
+    CHECK(dict.at(2) == 100);
+
+    dict[3];
+    REQUIRE(dict.size() == 2);
+
+    dict[3] = 200;
+    REQUIRE(dict.size() == 2);
+    CHECK(dict[3] == 200);
+    CHECK(dict.at(3) == 200);
+}
+
+TEST_CASE("Element at assignment") {
+    auto dict = dictcpp::Dict<int, int>();
+    REQUIRE(dict.empty());
+
+    dict.at(2) = 20;
+    REQUIRE(dict.size() == 1);
+    CHECK(dict[2] == 20);
+    CHECK(dict.at(2) == 20);
+
+    dict.at(2) = 100;
+    REQUIRE(dict.size() == 1);
+    CHECK(dict[2] == 100);
+    CHECK(dict.at(2) == 100);
+
+    dict.at(2);
+    CHECK(dict.size() == 1);
+    CHECK(dict[2] == 100);
+    CHECK(dict.at(2) == 100);
+
+    dict.at(3);
+    REQUIRE(dict.size() == 2);
+
+    dict.at(3) = 200;
+    REQUIRE(dict.size() == 2);
+    CHECK(dict[3] == 200);
+    CHECK(dict.at(3) == 200);
+}
+
+TEST_CASE("Element access") {
+    const auto dict = [] {
+        auto dict1 = dictcpp::Dict<int, int>();
+        dict1[2] = 20;
+        dict1[3] = 100;
+        return dict1;
+    }();
+
+    REQUIRE(dict.size() == 2);
+    CHECK(dict[2] == 20);
+    CHECK(dict.at(2) == 20);
+
+    CHECK(dict[3] == 100);
+    CHECK(dict.at(3) == 100);
 }
