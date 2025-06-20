8df91006afab6058a46eea3c29dab7f804df3025
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-06-20 09:25:05 +0100

    Keys, values, and items

--- dictcpp.hpp
+++ dictcpp.hpp
@@ -4,53 +4,74 @@
 #include <vector>
 
 namespace dictcpp {
-template<typename Key, typename Value>
+template<typename Key, typename Value = Key>
+struct Item {
+    Key key;
+    Value value;
+};
+
+template<typename Key, typename Value = Key>
 class Dict {
-    std::vector<Key> keys;
-    std::vector<Value> values;
+    std::vector<Key> key_list;
+    std::vector<Value> val_list;
+
+    bool key_exists(const Key &key) const {
+        return std::set<Key>(key_list.begin(), key_list.end()).contains(key);
+    }
 
     size_t get_index(const Key &key) const {
-        for (size_t i = 0; i < keys.size(); i++) {
-            if (keys[i] == key) {
+        for (size_t i = 0; i < key_list.size(); i++) {
+            if (key_list[i] == key) {
                 return i;
             }
         }
-
         throw std::logic_error("`get_index` called but key not in dictionary");
     }
 
 public:
     // Initialise an empty dictionary
-    Dict(): keys({}), values({}) {
+    Dict(): key_list({}), val_list({}) {
+    }
+
+    explicit Dict(const std::vector<Item<Key, Value>> &key_values) : key_list(), val_list() {
+        for (const auto &kv: key_values) {
+            if (key_exists(kv.key)) {
+                const auto index = get_index(kv.key);
+                val_list[index] = kv.value;
+            } else {
+                key_list.emplace_back(kv.key);
+                val_list.emplace_back(kv.value);
+            }
+        }
     }
 
     [[nodiscard]] bool empty() const {
-        return keys.empty();
+        return key_list.empty();
     }
 
     [[nodiscard]] size_t size() const {
-        return keys.size();
+        return key_list.size();
     }
 
     const Value &operator[](const Key &key) const {
-        if (!std::set<Key>(keys.begin(), keys.end()).contains(key)) {
+        if (!key_exists(key)) {
             throw std::out_of_range("Key not found in dictionary");
         }
 
-        return values[get_index(key)];
+        return val_list[get_index(key)];
     }
 
     Value &operator[](const Key &key) {
-        if (std::set<Key>(keys.begin(), keys.end()).contains(key)) {
+        if (key_exists(key)) {
             const auto index = get_index(key);
 
-            return values[index];
+            return val_list[index];
         }
 
-        keys.push_back(key);
-        values.push_back(Value());
+        key_list.push_back(key);
+        val_list.push_back(Value());
 
-        return values.back();
+        return val_list.back();
     }
 
     Value &at(const Key &key) {
@@ -59,6 +80,23 @@ public:
 
     const Value &at(const Key &key) const {
         return operator[](key);
+    }
+
+    std::vector<Key> keys() const {
+        return key_list;
+    }
+
+    std::vector<Value> values() const {
+        return val_list;
+    }
+
+    std::vector<Item<Key, Value>> items() const {
+        std::vector<Item<Key, Value>> item_list;
+        for (int i = 0; i < key_list.size(); ++i) {
+            item_list.emplace_back(key_list[i], val_list[i]);
+        }
+
+        return item_list;
     }
 };
 }
--- tests/test_dict.cpp
+++ tests/test_dict.cpp
@@ -1,13 +1,16 @@
 #include "dictcpp.hpp"
 #include "tests/catch.hpp"
 
+using dictcpp::Item;
+using dictcpp::Dict;
+
 TEST_CASE("Empty dict") {
-    const auto dict = dictcpp::Dict<char, char>();
+    const auto dict = Dict<char, int>();
     REQUIRE(dict.empty());
 }
 
 TEST_CASE("Element assignment") {
-    auto dict = dictcpp::Dict<int, int>();
+    auto dict = Dict<int>();
     REQUIRE(dict.empty());
 
     dict[2] = 20;
@@ -35,7 +38,7 @@ TEST_CASE("Element assignment") {
 }
 
 TEST_CASE("Element at assignment") {
-    auto dict = dictcpp::Dict<int, int>();
+    auto dict = Dict<int>();
     REQUIRE(dict.empty());
 
     dict.at(2) = 20;
@@ -64,38 +67,97 @@ TEST_CASE("Element at assignment") {
 
 TEST_CASE("Element access") {
     const auto dict = [] {
-        auto dict1 = dictcpp::Dict<int, int>();
-        dict1[2] = 20;
-        dict1[3] = 100;
+        auto dict1 = Dict<char, int>();
+        dict1['2'] = 20;
+        dict1['3'] = 100;
         return dict1;
     }();
 
     REQUIRE(dict.size() == 2);
-    CHECK(dict[2] == 20);
-    CHECK(dict.at(2) == 20);
+    CHECK(dict['2'] == 20);
+    CHECK(dict.at('2') == 20);
 
-    CHECK(dict[3] == 100);
-    CHECK(dict.at(3) == 100);
+    CHECK(dict['3'] == 100);
+    CHECK(dict.at('3') == 100);
 
-    const auto value = dict[2];
+    const auto value = dict['2'];
     CHECK(value == 20);
-    const auto value2 = dict[3];
+    const auto value2 = dict['3'];
     CHECK(value2 == 100);
 
-    auto value3 = dict[2];
+    auto value3 = dict['2'];
     CHECK(value3 == 20);
     value3 = 100;
     CHECK(value3 == 100);
-    CHECK(dict[2] == 20);
-    CHECK(dict.at(2) == 20);
+    CHECK(dict['2'] == 20);
+    CHECK(dict.at('2') == 20);
 
-    auto value4 = dict[3];
+    auto value4 = dict['3'];
     CHECK(value4 == 100);
     value4 = 200;
     CHECK(value4 == 200);
-    CHECK(dict[3] == 100);
-    CHECK(dict.at(3) == 100);
+    CHECK(dict['3'] == 100);
+    CHECK(dict.at('3') == 100);
 
-    CHECK_THROWS_WITH(dict.at(5), "Key not found in dictionary");
-    CHECK_THROWS_WITH(dict[6], "Key not found in dictionary");
+    CHECK_THROWS_WITH(dict.at('5'), "Key not found in dictionary");
+    CHECK_THROWS_WITH(dict['6'], "Key not found in dictionary");
+}
+
+TEST_CASE("Sequence initializer") {
+    std::vector<Item<char>> values = {{'a', 'b'}, {'c', 'd'}, {'e', 'f'}};
+
+    const auto dict = Dict(values);
+
+    REQUIRE(dict.size() == values.size());
+
+    CHECK(dict['a'] == 'b');
+    CHECK(dict['c'] == 'd');
+    CHECK(dict['e'] == 'f');
+
+    values.emplace_back('a', 'g');
+    REQUIRE(values.back().key == 'a');
+    REQUIRE(values.back().value == 'g');
+
+    const auto dict2 = Dict(values);
+    REQUIRE(dict2.size() == dict.size());
+
+    CHECK(dict2['a'] == 'g');
+    CHECK(dict2['c'] == 'd');
+    CHECK(dict2['e'] == 'f');
+}
+
+TEST_CASE("Access iterators") {
+    const auto dict = Dict<int>({
+        Item{1, 2},
+        Item{3, 4},
+        Item{5, 6},
+        Item{7, 8},
+        Item{9, 10}
+    });
+
+    const auto keys = dict.keys();
+
+    REQUIRE(keys.size() == 5);
+    CHECK(keys[0] == 1);
+    CHECK(keys[1] == 3);
+    CHECK(keys[2] == 5);
+    CHECK(keys[3] == 7);
+    CHECK(keys[4] == 9);
+
+    const auto values = dict.values();
+    REQUIRE(values.size() == 5);
+    CHECK(values[0] == 2);
+    CHECK(values[1] == 4);
+    CHECK(values[2] == 6);
+    CHECK(values[3] == 8);
+    CHECK(values[4] == 10);
+
+    const auto items = dict.items();
+    REQUIRE(items.size() == 5);
+    size_t i = 0;
+    for (const auto &[key, value]: items) {
+        CHECK(key == keys[i]);
+        CHECK(value == values[i]);
+        i++;
+    }
 }
